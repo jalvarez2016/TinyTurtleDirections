9ba81571fe38af0654ab4cb77479af9922058f84
Jason <alvarezcoraiza@citypolyhs.org>
2017-01-18 21:05:02 +0000

    it was fun

---
+++ script.js
@@ -0,0 +1,14 @@
+TinyTurtle.apply(window);
+forward(40);
+right(90);
+forward(40);
+right(90);
+forward(40);
+right(90);
+forward(40);
+right(90);
+forward(40);
+right(45);
+forward(30);
+right(94);
+forward(30);
